bfb47c913c7566962e5860436ddf9240690802aa
mallimuondu <mallimuondu@gmail.com>
2020-10-15 17:45:25 +0300

    made it soo it cant be triked

--- subtraction.py
+++ subtraction.py
@@ -1,9 +1,15 @@
 def subtraction():
     number1 = int(input('insert first number: '))
+    if number1 == '' or number1 == ' ':
+        print('you have inputed the wrong thing')
     number2 = int(input('insert first number: '))
+    if number2 == '' or number2 == ' ':
+        print('you have inputed the wrong thing')
     c = number1-number2
     print('the answer is = ',c)
     d = input('do you want to try again (y/n): ')
     if d == 'y':
         subtraction()
+    elif d == '' or d == ' ':
+        print('you have inputed the wrong thing')
 subtraction()
