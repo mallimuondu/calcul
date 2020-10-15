ef7c968e040b57ad4db906f1c6426c92cb0ea05b
mallimuondu <mallimuondu@gmail.com>
2020-10-15 10:25:42 +0300

    i put my addition file in its branch

--- sqrt.py
+++
@@ -1,8 +0,0 @@
-def sqr():
-    number = int(input("enter a number to find the squre root: "))
-    sqrt = number ** 0.5
-    print("square root = :", sqrt)
-    d = input('do you want to try again (y/n): ')
-    if d == 'y':
-        sqr()
-sqr()
