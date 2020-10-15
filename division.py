d55b8a1e31b87318da03d34cb6fb74f915958b14
mallimuondu <mallimuondu@gmail.com>
2020-10-15 11:45:34 +0300

    i put my division file in its branch

--- division.py
+++ division.py
@@ -1,6 +1,20 @@
 def division():
-    number1 = int(input('insert first number: '))
-    number2 = int(input('insert first number: '))
+    try:
+        number1 = int(input('insert first number: '))
+        string_number1 = str(number1)
+        if number1 == '' and number1 = ' ':
+            print('you have inputed nothing try again')
+    except ValueError:
+        print ('That is not a number try again')
+        division()
+    try:
+        number2 = int(input('insert first number: '))
+        string_number2 = str(number2)
+        if number2 == '':
+            print('you have inputed nothing try again')
+    except ValueError:
+        print ('That is not a number try again')
+        division()
     c = number1/number2
     print('the answer is = ',c)
     d = input('do you want to try again (y/n): ')
