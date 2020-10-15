12bd7b1a3b3d6c801834a577012668b00af07647
mallimuondu <mallimuondu@gmail.com>
2020-10-15 17:06:31 +0300

    made it soo it cant be triked

--- addition.py
+++ addition.py
@@ -1,9 +1,15 @@
 def addition():
     number1 = int(input('insert first number: '))
+    if number1 == '' or number1 == ' ':
+        print('you have inputed nothing')
     number2 = int(input('insert first number: '))
+    if number2 == '' or number2 == ' ':
+        print('you have inputed nothing')
     c = number1+number2
     print('the answer is = ',c)
     d = input('do you want to try again (y/n): ')
     if d == 'y':
         addition()
+    elif d == '' or d == ' ':
+        print('you have inputed nothing')
 addition()
